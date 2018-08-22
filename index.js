33e886a917d649f6a2c00cc03a22af4982f82527
John Forrest <johnhforrest@outlook.com>
2018-08-22 16:24:04 -0500

    BREAKING: Updating to use vue-cli 3.0 rules and deps

--- index.js
+++ index.js
@@ -1,36 +1,91 @@
 module.exports = {
-  extends: [
-    '@codex-protocol/eslint-config-base',
-
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential',
-  ],
-
-  // check if imports actually resolve
-  settings: {
-    'import/resolver': {
-      webpack: {
-        config: 'build/webpack.base.conf.js'
-      }
-    }
-  },
-
+  root: true,
   env: {
-    browser: true,
+    node: true,
   },
+  extends: [
+    // This doesn't have to get defined in package.json because it
+    //  is imported automatically by cli-plugin-eslint in Vue CLI 3.0
+    'plugin:vue/essential',
 
-  // required to lint *.vue files
-  plugins: [
-    'vue'
+    // Required in package.json as an external dependency
+    '@vue/airbnb',
   ],
-
   rules: {
+    // disable max line length
+    'max-len': 'off',
+
+    // allow console.logs, base airbnb warns
+    'no-console': 'off',
+
+    // allow padded blocks
+    'padded-blocks': 'off',
+
+    // dissallow semi colons
+    semi: ['error', 'never'],
+
+    // allow variables that begin with _
+    'no-underscore-dangle': 'off',
+
+    // auto-add semi colons in front of lines that could cause problems (e.g.
+    //  open parens)
+    'semi-style': ['error', 'first'],
+
+    // this is actually redundant, already in base airbnb... will leave in case
+    //  they change it
+    'no-mixed-spaces-and-tabs': 'error',
+
+    // always requires parens around arrow function params
+    'arrow-parens': ['error', 'always'],
+
+    // always requires arrow function bodies {}
+    'arrow-body-style': ['error', 'always'],
+
+    // this could be removed I suppose. it allows you to write shorter while()
+    //  loops and such, which never really happens much anyway
+    'no-cond-assign': ['error', 'except-parens'],
+
+    // this is actually redundant, already in base airbnb... will leave in case
+    //  they change it
+    'comma-dangle': ['error', 'always-multiline'],
+
+    // allows unused vars in arguments, (e.g.
+    //  `new Promise((resolve, reject) => {} )` but you never use the reject)
+    //
+    // this is useful because it lets you know that an argument is available and
+    //  what it is even if you're not using it right now
+    'no-unused-vars': ['error', { args: 'none' }],
+
+    // they actually changed the base airbnb to be this exact rule, but they
+    //  haven't published it to npm yet?
+    'function-paren-newline': ['error', 'consistent'],
+
+    // this require you to write multiline comments with // like these comments
+    'multiline-comment-style': ['error', 'separate-lines'],
+
+    // allow i++ in for loops, base airbnb has this error for every use of ++
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+
+    // do not force array destructuring so you don't have to do
+    //  const [yeah] = response.result
+    //
+    //  instead of
+    //
+    //  const yeah = response.result[0]
+    //
+    // this also becomes a pain in the ass when you want the value at index 1
+    //  instead of 0
+    'prefer-destructuring': ['error', { array: false, object: true }],
+
+    // I think this basically allows you to write a single-line object with more
+    //  than 4 keys. airbnb default is this, but forces you to line break if
+    //  there are more than 4 keys
+    'object-curly-newline': ['error', { ObjectExpression: { multiline: true, consistent: true } }],
 
     // don't require .vue extension when importing
     'import/extensions': ['error', 'always', {
       js: 'never',
-      vue: 'never'
+      vue: 'never',
     }],
 
     // disallow reassignment of function parameters
@@ -41,21 +96,11 @@ module.exports = {
         'currentState', // for vuex state
         'state', // for vuex state
         'acc', // for reduce accumulators
-        'e' // for e.returnvalue
-      ]
+        'e', // for e.returnvalue
+      ],
     }],
-
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      optionalDependencies: ['test/unit/index.js']
-    }],
-
-    // disable dangling commas in functions since that seems to throw a parsing
-    //  error
-    'comma-dangle': ['error', 'always-multiline', { functions: 'never' }],
-
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-
-  }
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
 }
